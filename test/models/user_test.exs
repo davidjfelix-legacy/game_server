5d21c094b701a9fd4fe564378957224f7671b746
David J Felix <felix.davidj@gmail.com>
2016-05-08 18:51:48 -0400

    Mix generate user model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule GameServer.UserTest do
+  use GameServer.ModelCase
+
+  alias GameServer.User
+
+  @valid_attrs %{name: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
---
+++ web/views/user_view.ex
@@ -0,0 +1,3 @@
+defmodule GameServer.UserView do
+  use GameServer.Web, :view
+end
